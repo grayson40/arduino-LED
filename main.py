da4c910ec606a0b1af2a5003734aa666d48a1f38
Grayson Crozier <graysoncrozier40@gmail.com>
2022-04-23 11:18:46 -0400

    Implement console app

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # Importing Libraries
 import serial
 import time
+
 arduino = serial.Serial(port='COM8', baudrate=115200, timeout=.1)
 
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
+def write_to_serial(data):
+    arduino.write(bytes(data, 'utf-8'))
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
+    print(f'\n\tWrote "{data}" to COM8\n')
 
 
 while True:
     print('LED Patterns:\n\n1.) Color Wipe\n2.) Rainbow\n3.) Theater Chase\n4.) Stripes\n5.) Cylon\n')
-    num = input(">> ")  # Taking input from user
-    value = write_read(num)
+    data = input(">> ")  # Taking input from user
+    write_to_serial(data)
